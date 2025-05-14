9da4ad00343446c8f96a5da5bd411fea7957e4bd
sandeepkumaruttera <sandeepkumaruttera@gmail.com>
2025-05-14 09:01:10 +0530

    some-message

--- interview/numbers.sh
+++ interview/numbers.sh
@@ -3,11 +3,13 @@
 #set -x
 
 
-echo "enter a number"
+
+
+echo "enter the number"
 read x
 for i in {1..100};
 do
-if [ `expr $i % $x` == 0 ] || [ `expr $i % 10` == 0 ] && [ `expr $i % 15` != 0 ];
+if [ `expr $i % $x` -eq 0 ] || [ `expr $i % 5` -eq 0 ] && [ `expr $i % $x` -ne 0 ];
 then
     echo "$i"
 fi
